9b6b96922fa307ed18d3cf74847a09675bcaf538
FauziViko <152051797+fauziviko@users.noreply.github.com>
2024-04-19 20:06:24 +0700

    second commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const route = require("./route");
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT ||4000;
 
 app.use(express.json());
 app.use(
