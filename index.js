a624c57f67218c48fcfca205bc335088d3ed2516
FauziViko <152051797+fauziviko@users.noreply.github.com>
2024-04-24 20:42:51 +0700

    login

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ require("dotenv").config();
 const fileUpload = require("express-fileupload");
 const express = require("express");
 const route = require("./route");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT ||4000;
 
+app.use(cors());
 app.use(express.json());
 app.use(
     fileUpload({
